c80e212476f19e745a767bdc6ecf433e776b851e
Aleksandr Marchenko <marrchenko@gmail.com>
2015-04-24 20:10:50 +0300

    updating project

--- app/src/main/java/com/stenbergroom/material_tabs/app/MainActivity.java
+++ app/src/main/java/com/stenbergroom/material_tabs/app/MainActivity.java
@@ -1,10 +1,61 @@
 package com.stenbergroom.material_tabs.app;
 
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import com.astuetz.PagerSlidingTabStrip;
+
 /**
  * Created by Sten on 24.04.2015.
  */
-public class MainActivity  {
+public class MainActivity extends ActionBarActivity {
 
-    
+    @InjectView(R.id.toolbar)
+    Toolbar toolbar;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Material Tabs");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        ViewPager pager = (ViewPager) findViewById(R.id.viewpager);
+        pager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
+
+        PagerSlidingTabStrip tabs = (PagerSlidingTabStrip) findViewById(R.id.slidingtabs);
+        tabs.setViewPager(pager);
+    }
+
+    public class MyPagerAdapter extends FragmentPagerAdapter{
+
+        private final String[] TITLES = {"Index", "Message", "Explore", "Other", "Map"};
+
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return TITLES[position];        }
+
+        @Override
+        public int getCount() {
+            return TITLES.length;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return SuperAwesomeCardFragment.newInstance(position);
+        }
+    }
 }
---
+++ app/src/main/java/com/stenbergroom/material_tabs/app/SuperAwesomeCardFragment.java
@@ -0,0 +1,9 @@
+package com.stenbergroom.material_tabs.app;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Sten on 24.04.2015.
+ */
+public class SuperAwesomeCardFragment extends Fragment {
+}
